e3a6774608b5d5dd9cb2772ad3ca8db0111d3481
Snickser <snickser@gmail.com>
2024-05-04 09:28:33 +0300

    add

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024050300;
-$plugin->requires  = 2022112800;
+$plugin->version   = 2024050400;
+$plugin->requires  = 2023100900;
 $plugin->component = 'paygw_cryptocloud';
-$plugin->release   = '0.6';
+$plugin->release   = '0.7';
 $plugin->maturity  = MATURITY_STABLE;
