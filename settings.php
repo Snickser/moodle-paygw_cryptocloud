472c6d246e700f2946dbf3f23365ac62f0014e15
Snickser <snickser@gmail.com>
2024-12-13 09:05:29 +0300

    add

--- settings.php
+++ settings.php
@@ -25,6 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 if ($ADMIN->fulltree) {
+    $plugininfo = \core_plugin_manager::instance()->get_plugin_info('paygw_cryptocloud');
+    $donate = get_string('donate', 'paygw_cryptocloud', $plugininfo);
+    $mform->addElement('html', $donate);
+
     $settings->add(new admin_setting_heading('paygw_cryptocloud_settings', '', get_string('pluginname_desc', 'paygw_cryptocloud')));
 
     \core_payment\helper::add_common_gateway_settings($settings, 'paygw_cryptocloud');
